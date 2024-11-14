898fc0a794867cc3f7b86a1ad6e3d60f479e5ac7
TimotheSandt <timothe.sandt@edu.univ-fcomte.fr>
2024-11-14 14:23:43 +0100

    insertion de valeur

--- sql_projet.sql
+++ sql_projet.sql
@@ -100,3 +100,84 @@ CREATE TABLE utilise(
 SHOW TABLES;
 
 
+
+-- Individu;
+-- Piece;
+-- Type_reparation;
+-- Etat;
+-- Categorie_velo;
+-- Velo;
+-- Reparation;
+-- loue;
+-- utilise;
+
+INSERT INTO Individu (nom, prenom, adresse, telephone, email) 
+VALUES ('TALALI', 'Zakaria', '10 rue de la paix', '0606060606', 'Z4R4p@example.com'),
+      ('SANDT', 'Timothe', '11 rue de la paix', '0606060607', 'Z4R5p@example.com'),
+      ('MIGUET', 'Maxime', '12 rue de la paix', '0606060608', 'Z4R6p@example.com'),
+      ('DE CHEZ CARGLASS', 'Olivier', '13 rue de Carglass', '0310051515', 'carglass@carglass.com');
+
+
+INSERT INTO Piece (type_piece) 
+VALUES ('Roue'),
+      ('Chaine'),
+      ('Boulon'),
+      ('Pédale'),
+      ('Suspension'),
+      ('Frein'),
+      ('Batterie'),
+      ('Moteur');
+
+
+INSERT INTO Type_reparation (libelle_type_reparation) 
+VALUES ('Crevaison'),
+      ('Suspension'),
+      ('Frein'),
+      ('Pédalier'),
+      ('Changement de batterie'),
+      ('Entretien');
+
+
+INSERT INTO Etat (libelle_etat)
+VALUES ('Très mauvais'),
+      ('Mauvais'),
+      ('Moyen'),
+      ('Bon'),
+      ('Très bon'),
+      ('Neuf');
+
+
+INSERT INTO Categorie_velo (libelle_categorie_velo) 
+VALUES ('VTT'),
+      ('MTB'),
+      ('BMX'),
+      ('Vélo éléctrique'),
+      ('Vélo de course'),
+      ('Vélo de ville');
+
+
+INSERT INTO Velo (libelle_velo, code_categorie_velo, code_etat) 
+VALUES ('Jean-pierre bike', 1, 1),
+      ('biket', 2, 3),
+      ('carglass', 6, 5),
+      ('velo', 4, 5);
+
+
+INSERT INTO Reparation (date_reparation, duree_reparation, description_reparation, code_type_reparation, code_velo, identifiant_individu) 
+VALUES ('2022-01-01', 1, NULL, 4, 1, 1),
+      ('2022-02-02', 3, 'description', 2, 2, 2),
+      ('2023-03-03', 1, 'description', 3, 2, 1),
+      ('2024-04-04', 5, 'La batterie était mauvaise, elle a été remplacé', 5, 4, 4);
+
+INSERT INTO loue (identifiant_individu_bailleur, identifiant_individu_locataire, code_velo, JJMMAAAA, duree, prix) 
+VALUES (4, 2, 1, '2022-01-01', 1, 5.00),
+      (4, 1, 4, '2023-01-01', 21, 130.00),
+      (4, 4, 3, '2024-01-01', 31, 1500.00),
+      (4, 3, 2, '2025-01-01', 354464, -5800.00);
+
+
+INSERT INTO utilise (code_piece, code_reparation, date_utilisation, quantite) 
+VALUES (4, 1, '2022-01-01', 2),
+      (5, 2, '2022-02-02', 1),
+      (6, 3, '2023-03-03', 2),
+      (7, 4, '2024-04-04', 1);
