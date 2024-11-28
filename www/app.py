9e8e06fb86e10b8a8237b9bd087e7a5c39146303
TimotheSandt <timothe.sandt@edu.univ-fcomte.fr>
2024-11-28 10:19:21 +0100

    base code location

--- www/app.py
+++ www/app.py
@@ -76,7 +76,30 @@ def show_accueil():
     return render_template('layout.html')
 
 
-@app.route('/location/show')
+@app.route('/compte/add', methods=['GET'])
+def add_individu():
+    return render_template('individu/add_individu.html')
+
+
+@app.route('/compte/add', methods=['POST'])
+def valid_add_individu():
+    mycursor = get_db().cursor()
+    sql =   ''' INSERT INTO Individu(nom, prenom, adresse, telephone, email)
+                VALUES (%s, %s, %s, %s, %s);
+            '''
+    nom = request.form['nom']
+    prenom = request.form['prénom']
+    adresse = request.form['adresse']
+    telephone = request.form['telephone']
+    email = request.form['email']
+    values = (nom, prenom, adresse, telephone, email)
+    
+    mycursor.execute(sql, values)
+    get_db().commit()
+    return redirect('/')
+
+
+@app.route('/location/show', methods=['GET'])
 def show_location():
     mycursor = get_db().cursor()
     sql =   ''' SELECT Location.ID_location AS ID, Location.prix, Location.JJMMAAAA AS date, 
@@ -95,6 +118,97 @@ def show_location():
     return render_template('location/show_location.html', locations=locations)
 
 
+@app.route('/location/add', methods=['GET'])
+def add_location():
+    mycursor = get_db().cursor()
+    sql =   ''' SELECT code_velo, libelle_velo
+                FROM Velo
+                ORDER BY libelle_velo;
+            '''
+    mycursor.execute(sql)
+    velos = mycursor.fetchall()
+    
+    sql =   ''' SELECT identifiant_individu AS ID, CONCAT(nom, ' ', prenom) AS nom_prenom
+                FROM Individu;
+            '''
+    mycursor.execute(sql)
+    individus = mycursor.fetchall()
+    
+    return render_template('location/add_location.html', velos=velos, individus=individus)
+
+
+@app.route('/location/add', methods=['POST'])
+def valid_add_location():
+    prix = request.form['prix']
+    date = request.form['date']
+    duree = request.form['duree']
+    locataire = request.form['locataire']
+    bailleur = request.form['bailleur']
+    velo = request.form['velo']
+    
+    mycursor = get_db().cursor()
+    sql =   ''' INSERT INTO Location(prix, JJMMAAAA, duree, locataire, bailleur, code_velo)
+                VALUES (%s, %s, %s, %s, %s);
+            '''
+    values = (prix, date, duree, locataire, bailleur, velo)
+    mycursor.execute(sql, values)
+    get_db().commit()
+    return redirect(url_for('show_location'))
+
+
+@app.route('/location/edit/', methods=['GET'])
+def edit_location():
+    
+    # recherche de la location
+    id = request.args.get('id')
+    mycursor = get_db().cursor()
+    sql =   ''' SELECT ID_location AS ID, prix, JJMMAAAA AS date, duree, locataire, bailleur, code_velo
+                FROM Location
+                WHERE ID = %s;
+            '''
+    values = (id,)
+    mycursor.execute(sql, values)
+    location = mycursor.fetchone()
+    
+    # recherche des velos
+    mycursor = get_db().cursor()
+    sql =   ''' SELECT code_velo, libelle_velo
+                FROM Velo
+                ORDER BY libelle_velo;
+            '''
+    mycursor.execute(sql)
+    velos = mycursor.fetchall()
+    
+    # recherche des individus
+    sql =   ''' SELECT identifiant_individu AS ID, CONCAT(nom, ' ', prenom) AS nom_prenom
+                FROM Individu
+                ORDER BY nom_prenom;
+            '''
+    mycursor.execute(sql)
+    individus = mycursor.fetchall()
+    
+    return render_template('location/edit_location.html', location=location, velos=velos, individus=individus)
+
+
+@app.route('/location/edit', methods=['POST'])
+def valid_edit_location():
+    id = request.form['id']
+    prix = request.form['prix']
+    date = request.form['date']
+    duree = request.form['duree']
+    locataire = request.form['locataire']
+    bailleur = request.form['bailleur']
+    velo = request.form['velo']
+    
+    mycursor = get_db().cursor()
+    sql =   ''' UPDATE Location
+                SET prix = %s, JJMMAAAA = %s, duree = %s, locataire = %s, bailleur = %s, code_velo = %s
+                WHERE ID_location = %s;
+            '''
+    values = (prix, date, duree, locataire, bailleur, velo, id)
+    mycursor.execute(sql, values)
+    get_db().commit()
+    return redirect(url_for('show_location'))
 
 
 if __name__ == '__main__':
